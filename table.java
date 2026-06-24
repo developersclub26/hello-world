8a62163f4a3e16e9064dcb9328f89e5d816b0466
Rajesh_kasapogu <kasapogurajesh03@gmail.com>
2026-06-24 13:00:17 +0530

    Refine comments in table.java for clarity
    Updated comments to clarify functionality of the program.

--- table.java
+++ table.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class table {
  public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the Number: ");
-    int num = sc.nextInt();
+    System.out.print("Enter the Number: ");// Matematical tables 
+    int num = sc.nextInt();// based on input it gives the tables
     {
         for(int i=1; i<=10;i++)
         {
